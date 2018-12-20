721229fa9009bbe2a7797e801be383fe2d68dee3
Jaked <530974640@qq.com>
2018-12-20 11:17:04 +0800

    add ErrorBoundary; udpate antd

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import history from '@/utils/history';
 import app from '@/views/app';
 import { Provider } from 'react-redux';
 import store from '@/store';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 import 'animate.css';
 import 'font-awesome/css/font-awesome.min.css';
@@ -17,10 +18,12 @@ import '@/styles/app.scss';
 // });
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Route component={app} />
-        </Router>
-    </Provider>,
+    <ErrorBoundary>
+        <Provider store={store}>
+            <Router history={history}>
+                <Route component={app} />
+            </Router>
+        </Provider>
+    </ErrorBoundary>,
     document.getElementById('app')
 );
